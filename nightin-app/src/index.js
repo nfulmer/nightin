0ab7c37f9a874a8578f5f59ad8098258ba64593b
Nate <42274896+nfulmer@users.noreply.github.com>
2020-11-11 17:50:17 -0500

    added recipe puppy api reach
    react app + recipe puppy api reach

--- nightin-app/src/index.js
+++ nightin-app/src/index.js
@@ -15,3 +15,40 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister(); */
+
+//const axios = require('axios').default;
+
+window.onload = (event) => {
+  alert("This script is doing things!");
+  document.getElementById("onion").addEventListener("click", makeRequest);
+};
+
+async function makeRequest(ev){
+  let searchParam = ev.target.textContent;
+  alert(searchParam);
+
+  // let res = await fetch({
+  //   method: "GET",
+  //   url: "http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3",
+  // });
+
+  // alert(res.status);
+  // let stuff = document.createElement("p");
+  // stuff.textContent = res.data;
+  // document.getElementById("generate_content").append(stuff);
+
+  const res = await fetch("https://recipe-puppy.p.rapidapi.com/?p=1&i=onions&q=omelet", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
+		"x-rapidapi-key": "d02ad0adf9msh642490ab5e606bfp156cc1jsn12ec2adcd63d"
+	}
+}).then(response => {
+    alert(response.status);
+    return response.text();
+  }).then(function(data){
+    let stuff = document.createElement("p");
+    stuff.textContent = data;
+    document.getElementById("generate_content").append(stuff);
+  });
+};
