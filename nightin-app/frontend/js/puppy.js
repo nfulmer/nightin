187893ea53ba1fe6d519e2fbf3aa460d2ca4b8ff
Nima Subramaniam <46691643+nimasub@users.noreply.github.com>
2020-11-14 20:45:47 -0500

    Update puppy.js

--- nightin-app/frontend/js/puppy.js
+++ nightin-app/frontend/js/puppy.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-
-
->>>>>>> ede18cbf9755a268ae67ee68d46a5e1aa9210e22
-=======
-
-
->>>>>>> ede18cbf9755a268ae67ee68d46a5e1aa9210e22
 export async function makeRequest(url){
     let result;
   
@@ -17,36 +7,14 @@ export async function makeRequest(url){
           "x-rapidapi-host": "recipe-puppy.p.rapidapi.com",
           "x-rapidapi-key": "d02ad0adf9msh642490ab5e606bfp156cc1jsn12ec2adcd63d"
       }
-<<<<<<< HEAD
-<<<<<<< HEAD
 }).then(response => {
       //alert(response.status);
       result = response.text();
     });
     return result;
-=======
-=======
->>>>>>> ede18cbf9755a268ae67ee68d46a5e1aa9210e22
-  }).then(response => {
-      //alert(response.status);
-      result = response.text();
-     });
-     return result;
-<<<<<<< HEAD
->>>>>>> ede18cbf9755a268ae67ee68d46a5e1aa9210e22
-=======
->>>>>>> ede18cbf9755a268ae67ee68d46a5e1aa9210e22
      //.then(function(data){
     //   let stuff = document.createElement("p");
     //   stuff.textContent = data;
     //   document.getElementById("generate_content").append(stuff);
     // });
-<<<<<<< HEAD
-<<<<<<< HEAD
 };
-=======
-  };
->>>>>>> ede18cbf9755a268ae67ee68d46a5e1aa9210e22
-=======
-  };
->>>>>>> ede18cbf9755a268ae67ee68d46a5e1aa9210e22
